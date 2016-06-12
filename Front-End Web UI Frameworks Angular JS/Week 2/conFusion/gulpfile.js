7c9e04982030ad28cb5860c95631b2c890e83c23
mcheli <mpcheli7@gmail.com>
2016-06-12 16:54:48 -0400

    Starting angular assignment 2

--- Front-End Web UI Frameworks Angular JS/Week 2/conFusion/gulpfile.js
+++ Front-End Web UI Frameworks Angular JS/Week 2/conFusion/gulpfile.js
@@ -12,6 +12,7 @@ var gulp = require('gulp'),
     changed = require('gulp-changed'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
+    ngannotate = require('gulp-ng-annotate'),
     del = require('del');
 
 gulp.task('jshint', function() {
@@ -34,7 +35,7 @@ gulp.task('usemin',['jshint'], function () {
     return gulp.src('./app/menu.html')
         .pipe(usemin({
             css:[minifycss(),rev()],
-            js: [uglify(),rev()]
+            js: [ngannotate(),uglify(),rev()]
         }))
         .pipe(gulp.dest('dist/'));
 });

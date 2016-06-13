8feb4d3fdb84348c2a58802c3cdc7fdfd6c505ad
mcheli <mpcheli7@gmail.com>
2016-06-13 15:43:25 -0400

    NodeJS week 1, 3/4 lecture progress

---
+++ Server-side Development with NodeJS/Week 1 - Introduction to Server-side Development/node-examples/rectangle-1.js
@@ -0,0 +1,7 @@
+exports.perimeter = function (x, y) {
+    return (2*(x+y));
+}
+
+exports.area = function (x, y) {
+    return (x*y);
+}
---
+++ Server-side Development with NodeJS/Week 1 - Introduction to Server-side Development/node-http/server-1.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var server = http.createServer(function(req, res){
+    console.log(req.headers);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<html><body><h1>Hello World</h1></body></html>');
+})
+server.listen(port, hostname, function(){
+    //noinspection JSAnnotator
+    console.log(`Server running at http://${hostname}:${port}/`);
+});

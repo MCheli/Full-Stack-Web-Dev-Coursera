11a933805f707d091cc2e95d34764d4ef2a97f90
mcheli <mpcheli7@gmail.com>
2016-06-26 22:07:46 -0400

    Week 3 NodeJS, finished first exercise

---
+++ Server-side Development with NodeJS/Week 3/rest-server/models/leadership.js
@@ -0,0 +1,37 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+// make this available to our Node applications
+module.exports = Leaders;
